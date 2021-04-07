dd1338e1c71d8bd3f819a4cbe30080f0b1eb52fd
Mike <michaeldeanmdmph@gmail.com>
2021-04-06 21:12:32 -0700

    user auth almost finished, end of bench bnb1 part1

--- frontend/tomo_book.jsx
+++ frontend/tomo_book.jsx
@@ -4,32 +4,20 @@ import {login, signup, logout} from './util/session_api_util'
 import configureStore from '../frontend/store/store'
 import Root from "./components/root"
 document.addEventListener('DOMContentLoaded', () => {
-  
-  const store = configureStore();
   const root = document.getElementById('root');
-  // const mike = {id: 123, username: 'mike', password:'123456', email:'123', birthday: '20200101'};
-  // window.logout = logout;
-  // window.signup = signup;
-  // window.login = login;
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
-  // window.login(mike);
-  ReactDOM.render(<Root store={store}/>, root);
   
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  ReactDOM.render(<Root store={store}/>, root);  
 });
-// {
-//   entities: {
-//     users: {
-//       1: {
-//         id: 1,
-//         username: 'breakfast'
-//       }
-//     }
-//   },
-//   session: {
-//     id: 1
-//   },
-//   errors: {
-//     session: []
-//   }
-// }
