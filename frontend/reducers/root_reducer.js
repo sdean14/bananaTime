e99745c8d52691de999ef70f162d9e5ccad37868
satomi <satomiampuku2@gmail.com>
2021-05-10 22:12:23 -0700

    comment root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,12 +2,15 @@ import { combineReducers } from "redux";
 import entities from './entities_reducer';
 import session from './session_reducer';
 import errors from './errors_reducer';
-import posts from './posts_reducer'
+import posts from './posts_reducer';
+import comments from './comments_reducer';
+
 const rootReducer = combineReducers({
     entities,
     session,   
     errors,
     posts,
+    comments,
 });
 
 export default rootReducer;
