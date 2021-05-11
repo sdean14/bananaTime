564cd017d7514a587f10c5bfdcb291da61ef6e8c
satomi <satomiampuku2@gmail.com>
2021-05-10 21:54:47 -0700

    comment util

---
+++ frontend/util/comment_api.util.js
@@ -0,0 +1,35 @@
+
+export const fetchComments = () => (
+  $.ajax({
+    url: `api/comments/`
+  })
+);
+
+export const fetchComment = postId => (
+  $.ajax({
+    url: `/api/comments/${postId}/`
+  })
+);
+
+export const createComment = comment => (
+  $.ajax({
+    url: `api/comments/`,
+    method: 'POST',
+    data: { comment }
+  })
+);
+
+export const updateComment = comment => (
+  $.ajax({
+    url: `/api/comments/${comment.id}`,
+    method: 'PATCH',
+    data: { comment }
+  })
+);
+
+export const deleteComment = commentId => (
+  $.ajax({
+    url: `/api/comments/${commentId}/`,
+    method: 'DELETE'
+  })
+)
