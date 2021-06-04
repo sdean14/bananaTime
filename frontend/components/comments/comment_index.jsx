acf2aab45e75cea7a353d1c768bb03bbc99b37c7
satomi <satomiampuku2@gmail.com>
2021-06-04 13:24:08 -0700

    remove comment out

--- frontend/components/comments/comment_index.jsx
+++ frontend/components/comments/comment_index.jsx
@@ -16,25 +16,23 @@ class CommentIndex extends React.Component {
 
       return (
         <ul>
-          {
-            this.props.comments.map((comment, idx) => (
-              <li className='each-comment' key={idx}>
+          {this.props.comments.map((comment, idx) => (
+            <li className='each-comment' key={idx}>
 
-                <Link to={`/users/${comment.commenter_id}/show`}>
-                  <div className='post-prof-icon'>{this.props.users[comment.commenter_id].photoUrl ? <img src={this.props.users[comment.commenter_id].photoUrl} alt={comment.commenter_id} /> : <div className='post-default-icon'></div>}</div>
-                </Link>
+              <Link to={`/users/${comment.commenter_id}/show`}>
+                <div className='post-prof-icon'>{this.props.users[comment.commenter_id].photoUrl ? <img src={this.props.users[comment.commenter_id].photoUrl} alt={comment.commenter_id} /> : <div className='post-default-icon'></div>}</div>
+              </Link>
 
-                <div className='comment-name-text'>
+              <div className='comment-name-text'>
                 <Link className='name' to={`/users/${comment.commenter_id}/show`}>
                   <p>{this.props.users[comment.commenter_id] ? this.props.users[comment.commenter_id].username : null}</p></Link>
-                  <p>{comment.body}</p>
-                </div>
-                {comment.commenter_id === this.props.currentUser.id ? 
-                <i className="far fa-trash-alt" onClick={() => this.props.deleteComment(comment.id)}></i> : null }
+                <p>{comment.body}</p>
+              </div>
+              {comment.commenter_id === this.props.currentUser.id ?
+                <i className="far fa-trash-alt" onClick={() => this.props.deleteComment(comment.id)}></i> : null}
 
-              </li>
-            ))
-          }
+            </li>
+          ))}
         </ul>
       )
     }
