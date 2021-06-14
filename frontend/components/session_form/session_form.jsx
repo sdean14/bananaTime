42c034e3db69efe51c778a49efa3787d7ca8a3e4
satomi <satomiampuku2@gmail.com>
2021-06-13 19:15:39 -0700

    remove comment out

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -62,7 +62,7 @@ class SessionForm extends React.Component {
             required />
           <br />
           <label>Birthday:
-          <input type="date"
+            <input type="date"
               value={this.state.birthday}
               onChange={this.update('birthday')}
               className="login-input"
@@ -77,49 +77,49 @@ class SessionForm extends React.Component {
     return (
       <div>
 
-      <div className='header-for-form'>
-        <div className='splash-header'>
-          <h1 className='splash-logo'>Tomobook</h1>
-          <h2 className='splash-title'>Connect with friends and the world around you on Tomobook.</h2>
-        </div>
+        <div className='header-for-form'>
+          <div className='splash-header'>
+            <h1 className='splash-logo'>Tomobook</h1>
+            <h2 className='splash-title'>Connect with friends and the world around you on Tomobook.</h2>
+          </div>
 
 
-        <div className="login-form-container">
-          <form onSubmit={this.handleSubmit} className="login-form-box">
-            <br />
-            {this.renderErrors()}
-            <div className="login-form">
+          <div className="login-form-container">
+            <form onSubmit={this.handleSubmit} className="login-form-box">
               <br />
-              <input type="text"
-                placeholder="Email"
-                value={this.state.email}
-                onChange={this.update('email')}
-                className="login-input"
-                required />
-              <br />
-              <input type="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                className="login-input"
-                required />
-              {this.renderSignup()}
-              <br />
-              <button type="submit" className="session-submit" >{this.props.formType}</button>
-            </div>
-            <button className="demo-button" onClick={this.handleDemo}>Demo</button>
-          </form>
-          <div className='signup-border'></div>
-          {this.props.navLink}
+              {this.renderErrors()}
+              <div className="login-form">
+                <br />
+                <input type="text"
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.update('email')}
+                  className="login-input"
+                  required />
+                <br />
+                <input type="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  className="login-input"
+                  required />
+                {this.renderSignup()}
+                <br />
+                <button type="submit" className="session-submit" >{this.props.formType}</button>
+              </div>
+              <button className="demo-button" onClick={this.handleDemo}>Demo</button>
+            </form>
+            <div className='signup-border'></div>
+            {this.props.navLink}
+          </div>
         </div>
+        <footer className='footer-copyright'>
+          <p> ©Satomi Dean  2021</p>
+          <p ></p>
+          <img className='githubcat' src="https://cdn2.iconfinder.com/data/icons/social-icons-grey/512/GITHUB-512.png" alt="" />
+          <a href="https://github.com/sdean14">GitHub</a>
+        </footer>
       </div>
-      <footer className='footer-copyright'>
-        <p> ©Satomi Dean  2021</p>
-        <p ></p>
-        <img className='githubcat' src="https://cdn2.iconfinder.com/data/icons/social-icons-grey/512/GITHUB-512.png" alt=""/>
-        <a href="https://github.com/sdean14">GitHub</a>
-      </footer>
-    </div>
     );
   }
 }
--- frontend/components/users/user_index_item.jsx
+++ frontend/components/users/user_index_item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const UserIndexItem = ({ user }) => (
   <ul>
-      <Link to={`/users/${user.id}/show`}><li className='index-each-user' >{user.username}</li></Link>
+    <Link to={`/users/${user.id}/show`}><li className='index-each-user' >{user.username}</li></Link>
   </ul>
 );
 
