7a8e4be8d9eb4b08782e5cf3f8801c487239f0bd
satomi <satomiampuku2@gmail.com>
2021-06-18 12:54:24 -0700

    remove comment out

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Route,  Switch,  Link} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import LogInFormContainer from './session_form/login_form_container';
 import SignUpFormContainer from './session_form/signup_form_container';
