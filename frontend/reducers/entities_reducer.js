3e75d372bb7a5171bffcd86051ffe9ba87e4cacc
satomi <satomiampuku2@gmail.com>
2021-06-19 14:23:29 -0700

    remove comment out

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 import users from './users_reducer';
 import posts from './posts_reducer';
-import friendships from './friendship_reducers'
+import friendships from './friendship_reducers';
 const entitiesReducer = combineReducers({
     users,
     friendships,
