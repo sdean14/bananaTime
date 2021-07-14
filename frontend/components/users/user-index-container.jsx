0a488b1893c57bf6b6f85d7aa9fcde23de7f11b3
satomi <satomiampuku2@gmail.com>
2021-07-13 23:00:46 -0700

    clean empty space

--- frontend/components/users/user-index-container.jsx
+++ frontend/components/users/user-index-container.jsx
@@ -4,7 +4,6 @@ import UsertIndex from './user-index';
 import { fetchUser, fetchUsers } from '../../actions/user_actions';
 
 const mSTP = (state) => {
-
   return({
     currentUser: state.entities.users[state.session.id],
     users: Object.values(state.entities.users),
