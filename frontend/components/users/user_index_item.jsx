2876223f3d97e97980923cc50849f87f4054d0df
satomi <satomiampuku2@gmail.com>
2021-07-13 22:59:24 -0700

    clean empty space

--- frontend/components/users/user_index_item.jsx
+++ frontend/components/users/user_index_item.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 const UserIndexItem = ({ user }) => (
   <ul>
     <Link to={`/users/${user.id}/show`}><li className='index-each-user' >{user.username}</li></Link>
