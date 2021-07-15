7f01cc58bb050370e5d3859c67644be280bb5c75
satomi <satomiampuku2@gmail.com>
2021-07-14 19:23:32 -0700

    clean empty space

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,14 +1,13 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_action';
 import { RECEIVE_USERS, RECEIVE_USER } from '../actions/user_actions';
 
-const usersReducer = (oldState={}, action) => {
+const usersReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
     const newState = Object.assign({}, oldState);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             newState[action.currentUser.id] = action.currentUser
             return newState;
-            // return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
         case RECEIVE_USERS:
             return Object.assign({}, oldState, action.users)
         case RECEIVE_USER:
