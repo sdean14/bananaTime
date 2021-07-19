f3932a3d130cd174d80467dd94e1408e979d9aa3
satomi <satomiampuku2@gmail.com>
2021-07-19 12:17:58 -0700

    clean space, comment

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,16 +5,16 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-      @user = User.new(user_params) 
-  
-      if @user.save
-        Friendship.create(requester_id: @user.id, requested_id: @user.id)
-        login(@user)
-        render "api/users/show"
-      else
-        render json: @user.errors.full_messages, status: 422
-      end
+    @user = User.new(user_params) 
+
+    if @user.save
+      Friendship.create(requester_id: @user.id, requested_id: @user.id)
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
     end
+  end
 
   def show
     @user = User.find(params[:id])
@@ -52,7 +52,6 @@ class Api::UsersController < ApplicationController
   # end
 
   private
-
   def user_params
     params.require(:user).permit(:username, :email, :birthday, :password, :photo, :location, :work)
   end
